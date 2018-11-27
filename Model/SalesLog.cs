e260f8d9dbd4cf29cd27e3f98bd0cf02d244e809
Nivedita Swaminathan <atid1903@gmail.com>
2018-11-27 11:43:56 +0100

    Add files via upload

---
+++ Model/SalesLog.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ImportCSV
+{
+	[Table("theBigStorage")]
+	public class SalesLog
+	{
+		[Key]
+		public int RecordId { get; set; }
+
+		[StringLength(3, MinimumLength = 3)]
+		public string ShopCode { get; set; }
+
+		public int ItemId { get; set; }
+
+		public string ItemName { get; set; }
+
+		public decimal PricePerItem { get; set; }
+
+		public int CountOfItems { get; set; }
+
+		public decimal TotalPrice { get; set; }
+
+		public DateTime TransactionDate { get; set; }
+	}
+}
---
+++ Repository/SalesLogRepository.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ImportCSV
+{
+	public class SalesLogRepository
+	{
+		private readonly StorageContext _storageContext;
+		private readonly string _connectionString;
+
+		private string errorLog;
+		/// <summary>
+		/// Gets the connection string and creates the storage context
+		/// </summary>
+		/// <param name="connectionString"></param>
+		public SalesLogRepository(string connectionString)
+		{
+			_connectionString = connectionString;
+			_storageContext = new StorageContext(connectionString);
+		}
+
+		/// <summary>
+		/// Insert multiple records into storage table
+		/// </summary>
+		/// <param name="storages"></param>		
+		public void InsertIntoMembers(DataTable salesTable)
+		{
+			using (var connection = new SqlConnection(_connectionString))
+			{
+				SqlTransaction transaction = null;
+				connection.Open();
+				try
+				{
+					transaction = connection.BeginTransaction();
+					using (var sqlBulkCopy = new SqlBulkCopy(connection, SqlBulkCopyOptions.TableLock, transaction))
+					{
+						sqlBulkCopy.BulkCopyTimeout = 60;
+						sqlBulkCopy.DestinationTableName = "theBigStorage";
+						sqlBulkCopy.ColumnMappings.Add("ShopCode", "ShopCode");
+						sqlBulkCopy.ColumnMappings.Add("ItemId", "ItemId");
+						sqlBulkCopy.ColumnMappings.Add("ItemName", "ItemName");
+						sqlBulkCopy.ColumnMappings.Add("PricePerItem", "PricePerItem");
+						sqlBulkCopy.ColumnMappings.Add("CountOfItems", "CountOfItems");
+						sqlBulkCopy.ColumnMappings.Add("TotalPrice", "TotalPrice");
+						sqlBulkCopy.ColumnMappings.Add("TransactionDate", "TransactionDate");
+
+						sqlBulkCopy.WriteToServer(salesTable);
+					}
+					transaction.Commit();
+				}
+				catch (Exception ex)
+				{
+					errorLog = ex.Message;
+					transaction.Rollback();
+					throw;
+				}
+			}
+		}
+	}
+}
